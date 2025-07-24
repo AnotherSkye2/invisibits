7017abdf9263c4a7e78a3bbe8972e2f262fdd544
skyehakomaa <skyehakomaa@gmail.com>
2025-07-24 16:57:02 +0300

    fix handling of too big images

--- static/handlers/UploadHandler.js
+++ static/handlers/UploadHandler.js
@@ -2,23 +2,23 @@ import { DisplayError } from "./DisplayError.js";
 
 export async function UploadHandler() {
     if (this.files && this.files[0]) {
-        var img = document.querySelector(`div[id=${this.id}] img`);
+        const img = document.querySelector(`div[id=${this.id}] img`);
         img.onload = () => {
             URL.revokeObjectURL(img.src);  // no longer needed, free memory
         }
         const maxSizeInMB = 5
         if(this.files[0].size / (1024 ** 2) > maxSizeInMB) {
+            const imgInput = document.querySelector(`div[id=${this.id}] .imgInput`)
             DisplayError("File size is too big!")
+            imgInput.value = ''
             return
         }
         
         img.src = URL.createObjectURL(this.files[0]);
-        console.log(img, img.height)
         const imgForm = document.querySelector(`div[id=${this.id}] #img-form`)
         const formData = new FormData(imgForm)
         formData.append("fileName", formData.get("imgFile").name)
         formData.delete("inputString")
-        console.log(...formData, imgForm)
         try {
             const response = await fetch(`./upload?`, {
                 method: "POST",
