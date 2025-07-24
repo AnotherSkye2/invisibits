40a53bcb54066c8ceff58fe9bbb113721aacda9d
skyehakomaa <skyehakomaa@gmail.com>
2025-07-24 19:13:45 +0300

    add error message timeout

--- static/handlers/DisplayError.js
+++ static/handlers/DisplayError.js
@@ -2,4 +2,7 @@ export function DisplayError(msg) {
     const errorMessage = document.querySelector('#errorMessage')
     errorMessage.innerHTML = msg
     console.error(msg)
+    setTimeout(() => {
+        errorMessage.innerHTML = ""
+    }, 3000)
 }
